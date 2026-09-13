8dbea48b75a922b8a7d4245ad498f97024f6f73d
Alef Loresca <lorescajustin@gmail.com>
2026-09-13 12:10:56 +0800

    Change portfolio icon to absolute path and remove third certificate

--- src/references/CertRef.tsx
+++ src/references/CertRef.tsx
@@ -1,5 +1,4 @@
 export const certSlides = [
   { src: './images/Certificates/Cert1.png', details: "freeCodeCamp's Front End Development Libraries Certificate, 2026" },
   { src: './images/Certificates/Cert2.jpg', details: "Great Learning's Front End Development Certificate, 2024" },
-  { src: './images/Certificates/Cert3.jpg', details: "On-The-Job Training (OJT) Completion Certificate, 2025" },
 ];
